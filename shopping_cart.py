87e64349e8ed0e9ff1aa47ca6e173d178c23d434
Camuz <P00129197@houstonisd.org>
2019-04-07 13:05:46 -0500

    all done

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,17 +1,31 @@
 class ShoppingCart:
     # write your code here
     def __init__(self, emp_discount=None):
-      pass
+        self.total=0
+        self.employee_discount=None
+        self.items=[]
+     
+    
     def add_item(self, name, price, quantity=1):
-       pass
+        self.items.append([name,price,quantity])
+        self.total+= price*quantity
+        print (self.items)
+        return self.total
+        pass
+    
+
+    
     def mean_item_price(self):
-       pass
+        mean = None
+        for i in self.items :
+            print ( self.items[i][1]) 
+        return mean
 
     def median_item_price(self):
         pass
 
     def apply_discount(self):
-       pass
+        pass
 
     def void_last_item(self):
         pass
