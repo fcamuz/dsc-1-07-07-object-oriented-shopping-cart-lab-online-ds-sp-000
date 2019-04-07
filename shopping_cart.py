b49886f58119df3f540949aa0897983bb9a2a215
Camuz <P00129197@houstonisd.org>
2019-04-07 13:14:19 -0500

    all done2

--- shopping_cart.py
+++ shopping_cart.py
@@ -2,30 +2,67 @@ class ShoppingCart:
     # write your code here
     def __init__(self, emp_discount=None):
         self.total=0
-        self.employee_discount=None
+        self.employee_discount=emp_discount
         self.items=[]
      
     
     def add_item(self, name, price, quantity=1):
         self.items.append([name,price,quantity])
         self.total+= price*quantity
-        print (self.items)
+      #  print (self.items)
         return self.total
-        pass
+        
     
-
     
     def mean_item_price(self):
-        mean = None
-        for i in self.items :
-            print ( self.items[i][1]) 
+        mean=0
+        for i in range(len(self.items)):
+            mean = mean + self.items[i][2]
+        mean= self.total/mean
         return mean
+    
 
     def median_item_price(self):
-        pass
+        med_price=[]
+        for i in range(len(self.items)):
+            k=1
+            if k==self.items[i][2]:
+                med_price.append(self.items[i][1])
+           
+            else:
+                for k in range(self.items[i][2]):
+                    med_price.append(self.items[i][1])
+                    
+        med_price.sort()
+        length=(len(med_price))
+
+        if length %2 == 0:
+            x= int(length/2)
+            return ((med_price[x-1]+med_price[x])/2)
+        else :
+            x=int((length-1)/2)
+            return(med_price[x])
+        
 
     def apply_discount(self):
-        pass
+        disc_total = self.total
+        if self.employee_discount==None:
+            return("Sorry, there is no discount to apply to your cart :(")
+        else:
+            disc_total= disc_total-disc_total*self.employee_discount/100
+            return(disc_total) 
+        #return self.total
+        
 
     def void_last_item(self):
-        pass
+        if self.items:
+            last_item=self.items[-1]
+            if last_item[2]>1:
+                self.total=self.total-last_item[1]
+                self.items[-1][2]=last_item[2]-1
+            else:
+                self.items.pop()
+                self.total=self.total-last_item[1]
+            return self.total
+        else:
+            print("There are no items in your cart")
